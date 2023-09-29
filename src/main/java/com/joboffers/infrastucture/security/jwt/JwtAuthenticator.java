e49f7e6d5fba0c68829cc87473057f3568dd8b6a
Agnieszka <agnieszka.ad77@gmail.com>
2023-09-29 16:40:48 +0200

    Created packages and facades for modules

---
+++ src/main/java/com/joboffers/infrastucture/security/jwt/JwtAuthenticator.java
@@ -0,0 +1,4 @@
+package com.joboffers.infrastucture.security.jwt;
+
+public class JwtAuthenticator {
+}
---
+++ src/test/java/com/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,12 @@
+package com.joboffers.domain.loginandregister;
+
+import org.junit.jupiter.api.Test;
+
+class LoginAndRegisterFacadeTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
